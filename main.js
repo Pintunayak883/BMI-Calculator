621f27a0ecc59c66334065dc34d8674892237a9a
Bhanwer <sarwanramji7172@gmail.com>
2023-12-20 16:04:42 +0530

    Virison 2.0

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 function cal() {
-    let h = document.getElementById("hinp").value;
-    let w = document.getElementById("winp").value;
-    let r = document.getElementById("result");
-    let bmi = w / (h / 100 * h / 100);
+  let h = document.getElementById("hinp").value;
+  let w = document.getElementById("winp").value;
+  let r = document.getElementById("result");
+  if (h === "" || w === "") {
+    r.innerHTML = "Please enter values for height and weight.";
+  } else if (isNaN(h) || isNaN(w)) {
+    r.innerHTML = "Please enter valid values for height and weight.";
+  } else {
+    let bmi = w / (((h / 100) * h) / 100);
     let total = bmi.toFixed(2);
-    let result = r.innerHTML = "Your BMI is " + total;
+    let result = (r.innerHTML = "Your BMI is " + total);
+  }
 }
